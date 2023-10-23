3197d9df71e2f6dc0877480b1bedccd95d95f1fa
Hoàng Xuân Khang <rkhang7@Hoangs-MacBook-Pro.local>
2023-10-23 22:04:46 +0700

    init project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hoàng Xuân Khang on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
